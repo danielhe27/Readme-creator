2c7e833b61f26023f886bb26f26eb8b52d3e90b6
danielhe27 <jdah49d@gmail.com>
2024-02-22 16:47:07 -0500

    final commit before comments

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge and link based on which license is passed in
+
 // If there is no license, return an empty string
 function renderLicenseInfo(license) {
   const licenseInfo = {
@@ -26,38 +26,70 @@ function renderLicenseInfo(license) {
 
   const selectedLicense = licenseInfo[license.toLowerCase()];
 
-  if (selectedLicense) {
-    return {
-      badge: selectedLicense.badge,
-      link: selectedLicense.link,
-    };
-  } else {
-    return {
-      badge: '',
-      link: '',
-    };
-  }
+  return selectedLicense ? selectedLicense.badge : '';
+}
+function renderLicenseBadge(license) {
+  const licenseBadges = {
+    MIT: '![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)',
+    Apache: '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)',
+    GPL: '!License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)',
+    BSD3: '![License: BSD 3](https://img.shields.io/badge/License-BSD%203-Clause-yellow.svg)',
+    None: '',
+  };
+  return licenseBadges[license] || '';
 }
 
-// TODO: Create a function that returns the license section of README
+function renderLicenseLink(license) {
+  const licenseInfo = {
+    MIT: {
+      badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)]',
+      link: 'https://opensource.org/licenses/MIT',
+    },
+    Apache: {
+      badge: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]',
+      link: 'https://opensource.org/licenses/Apache-2.0',
+    },
+    GPL: {
+      badge: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]',
+      link: 'https://www.gnu.org/licenses/gpl-3.0',
+    },
+    BSD: {
+      badge: '[![License: BSD 3](https://img.shields.io/badge/License-BSD%203-Clause-yellow.svg)]',
+      link: 'https://opensource.org/licenses/BSD-3-Clause',
+    },
+    None: {
+      badge: '',
+      link: '',
+    }
+  };
+  const selectedLicense = licenseInfo;
+
+  return selectedLicense ? selectedLicense.link : '';
+}
+
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+  const badge = renderLicenseBadge(license);
+  const link = renderLicenseLink(license);
+
   if (license) {
     return `## License
-  This project is licensed under the [${license} license](${renderLicenseInfo(license).link}).`;
+  This project is licensed under the [${license} license]`;
   } else {
     return '';
   }
 }
 
 
-// TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
-  const licenseInfo = renderLicenseSection(data.license[0]);;
+  const license = data.license[0]; // Extracting the license information from user input
+  const licenseSection = renderLicenseSection(license);
 
   return `# ${data.title}
 
-  ${licenseInfo.badge}
+  ${renderLicenseBadge(license)}
+
 
   ## Table of Contents
   - [Installation](#installation)
@@ -79,7 +111,9 @@ function generateMarkdown(data) {
   ## Tests
   ${data.tests}
 
-  ${licenseInfo.section}
+  ${licenseSection}
+
+
   
   ## Questions
   If you have any questions, please contact me:
